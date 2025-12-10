ed3172a8347dfda8edb1ad26c89b3de7f3d5e9c7
sridev <69075577+sridevkp@users.noreply.github.com>
2025-12-10 19:32:05 +0530

    refactor:use env

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,8 @@
 import io from "socket.io-client"
 
-const socket = io("https://chess-online-7wr7.onrender.com")
+const MATCHING_SERVER_URL = import.meta.env.VITE_MATCHING_SERVER_URL || "http://localhost:8080"
+
+const socket = io(MATCHING_SERVER_URL)
 socket.on("connect", data => {
     console.log("connected")
 })
